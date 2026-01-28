b3261082a65460a1ab68f73cca09eaac07ed644d
oldcast1e <kylhs0207@naver.com>
2026-01-28 18:11:30 +0900

    Profile Edit Function Update

--- mobile/app/(tabs)/profile.tsx
+++ mobile/app/(tabs)/profile.tsx
@@ -107,7 +107,7 @@ export default function ProfileScreen() {
         }
 
         let result = await ImagePicker.launchImageLibraryAsync({
-            mediaTypes: ImagePicker.MediaType.Images,
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
             allowsEditing: true,
             aspect: [1, 1],
             quality: 0.5,
